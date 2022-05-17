79a4c4d096a52e0f06a96aa530d777f24aee51f2
Tatiana2512 <hikari_neko@mail.ru>
2022-05-17 20:26:03 +0400

    add remote selenoid

--- src/test/java/ActionsTest.java
+++ src/test/java/ActionsTest.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -10,6 +13,17 @@ public class ActionsTest {
 
     SelenideElement a = $("#column-a");
     SelenideElement b = $("#column-b");
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.browserSize = "1400x800";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
 
     @Test
     void dragnDrop1stTest() {
